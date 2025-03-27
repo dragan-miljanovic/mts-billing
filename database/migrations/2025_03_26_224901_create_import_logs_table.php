824f1789e59cac05aa2ccfc929b29af4aface6ca
Dragan Miljanovic <dragan.miljanovic@q.agency>
2025-03-27 22:15:59 +0100

    import logs table updated

--- database/migrations/2025_03_26_224901_create_import_logs_table.php
+++ database/migrations/2025_03_26_224901_create_import_logs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('uid')->unique();
             $table->integer('total_chunks')->default(0);
-            $table->integer('imported')->default(0);
+            $table->integer('inserted')->default(0);
             $table->integer('updated')->default(0);
             $table->timestamps();
         });
