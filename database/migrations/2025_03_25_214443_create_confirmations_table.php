760ed00ca9c3756ebb3cc9465f7f262ac03405d4
Dragan Miljanovic <dragan.miljanovic@q.agency>
2025-03-29 22:17:58 +0100

    migrations for call charges and confirmations refactor for mandatory properties

--- database/migrations/2025_03_25_214443_create_confirmations_table.php
+++ database/migrations/2025_03_25_214443_create_confirmations_table.php
@@ -13,21 +13,21 @@ return new class extends Migration
     {
         Schema::create('confirmations', function (Blueprint $table) {
             $table->id();
-            $table->string('record_version', 8)->nullable();
-            $table->string('crce_operation', 32)->nullable();
+            $table->string('record_version', 8);
+            $table->string('crce_operation', 32);
             $table->string('active_feature', 64)->nullable();
             $table->integer('sequence_total')->nullable();
             $table->string('bundle_code', 32)->nullable();
             $table->bigInteger('opp_id')->nullable();
             $table->string('service_type', 64)->nullable();
             $table->string('customer_care_user', 32)->nullable();
-            $table->string('subscriber_language', 64)->nullable();
-            $table->string('imsi', 32)->nullable();
-            $table->string('account_status', 32)->nullable();
-            $table->bigInteger('tariff')->nullable();
+            $table->string('subscriber_language', 64);
+            $table->string('imsi', 32);
+            $table->string('account_status', 32);
+            $table->bigInteger('tariff');
             $table->bigInteger('new_tariff')->nullable();
             $table->bigInteger('pool_id')->nullable();
-            $table->bigInteger('transaction_fee')->nullable();
+            $table->bigInteger('transaction_fee');
             $table->bigInteger('old_value')->nullable();
             $table->bigInteger('new_value')->nullable();
             $table->string('currency', 64)->nullable();
@@ -46,8 +46,8 @@ return new class extends Migration
             $table->dateTimeTz('subscriber_activation_date', 0)->nullable();
             $table->dateTimeTz('subscriber_expiry_date', 0)->nullable();
             $table->string('cost_control_limit_name', 32)->nullable();
-            $table->string('revenue_code', 32)->nullable();
             $table->string('crce_result_code', 128)->nullable();
+            $table->string('revenue_code', 32)->nullable();
             $table->string('transparent_data', 1024);
             $table->timestamps();
         });
