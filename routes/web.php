ba21292480a9bf6b6a29e7a2818d4e0216257b59
Dragan Miljanovic <dragan.miljanovic@q.agency>
2025-03-29 14:38:59 +0100

    routes refactor

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,14 @@ use Illuminate\Support\Facades\Route;
 
 //Home
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+//Import
+Route::get('/index', [ImportController::class, 'index'])->name('import.index');
 Route::post('/import', [ImportController::class, 'import'])->name('import');
+
+//PDF
 Route::get('/pdf', [PdfController::class, 'generate'])->name('generate.pdf');
 
+//Resources
 Route::resource('call-charges', CallChargeController::class)->only(['index', 'show', 'destroy']);
 Route::resource('confirmations', ConfirmationController::class)->only(['index', 'show', 'destroy']);
