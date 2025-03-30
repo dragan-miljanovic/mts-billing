c33e28f78ff846bd86e3ce5cfaa7d7b4be99916f
Dragan Miljanovic <dragan.miljanovic@q.agency>
2025-03-30 17:04:42 +0200

    public/storage remove from gitignore

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
     <div class="container text-center mt-5">
-        <!-- MTS Mobilna Telefonija Logo Section -->
+        <!-- MTS SI Logo Section -->
         <div class="row">
             <div class="col-12">
-                <img src="https://upload.wikimedia.org/wikipedia/sr/thumb/f/f0/%D0%9C%D0%A2%D0%A1_%28%D0%BB%D0%BE%D0%B3%D0%BE_2015%29.svg/800px-%D0%9C%D0%A2%D0%A1_%28%D0%BB%D0%BE%D0%B3%D0%BE_2015%29.svg.png?20241121010338"
-                     alt="MTS Mobilna Telefonija Logo" class="img-fluid" style="max-width: 300px;">
+                <img src="{{ asset('storage/mts-si-tp.png') }}"
+                     alt="MTS SI Logo" class="img-fluid">
             </div>
         </div>
 
         <!-- Heading -->
         <div class="row mt-4">
             <div class="col-12">
-                <h2 class="display-4 text-danger">Welcome to MTS Mobilna Telefonija</h2>
-                <p class="lead text-secondary">Your trusted provider for mobile services and solutions.</p>
+                <h2 class="display-4 text-danger">Welcome to MTS SI</h2>
+                <p class="lead text-secondary">Povezujemo tehnologiju i budućnost. Na pravom SI mestu.</p>
             </div>
         </div>
 
@@ -22,17 +22,8 @@
         <div class="row mt-5">
             <div class="col-12">
                 <p class="text-muted">
-                    MTS Mobilna Telefonija is your one-stop solution for mobile services. We provide high-quality telecommunications and mobile solutions to keep you connected wherever you go.
+                    Neka te tvoja sledeća digitalna avantura poveže sa nama. Hajde da zajedno stvaramo budućnost.
                 </p>
-            </div>
-        </div>
-
-        <!-- Call to Action Button -->
-        <div class="row mt-4">
-            <div class="col-12">
-                <a href="https://mts.rs" class="btn btn-danger btn-lg">
-                    <i class="fa fa-mobile-alt"></i> Explore Our Services
-                </a>
             </div>
         </div>
     </div>
